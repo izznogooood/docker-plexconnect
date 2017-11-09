5e20f7edf693d6a2da3bb8e9bfc2bb02e154f893
Anders <a.m.andersen@me.com>
2017-11-09 19:22:02 +0100

    changed to phusion/baseimage

--- initPlexconnect.sh
+++ initPlexconnect.sh
@@ -3,15 +3,16 @@
 useradd -m $USERID -u $USERID
 groupadd $GROUPID
 #If this is the first time you run the docker, clone (get) PlexConnect, else update
-if [ ! -f /PlexConnect/ATVSettings.cfg ]; 
+if [ ! -f /PlexConnect/ATVSettings.cfg ];
     then
+        cd /
         git clone https://github.com/iBaa/PlexConnect.git
-    else 
-        cd PlexConnect
+    else
+        cd /PlexConnect
         git pull
 fi
 #If this is the first time, generate certificates
-if [ ! -f /PlexConnect/assets/certificates/trailers.pem ]; 
+if [ ! -f /PlexConnect/assets/certificates/trailers.pem ];
     then
         openssl req -new -nodes -newkey rsa:2048 -out ./trailers.pem -keyout ./trailers.key -x509 -days 7300 -subj "/C=US/CN=trailers.apple.com"
         openssl x509 -in ./trailers.pem -outform der -out ./trailers.cer && cat ./trailers.key >> ./trailers.pem
@@ -21,4 +22,3 @@ fi
 chown -R $USERID:$GROUPID /PlexConnect
 #Start PlexConnect
 python /PlexConnect/PlexConnect.py
-
